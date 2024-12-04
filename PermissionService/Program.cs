745f27b67e339bddd8624f87ae2cbe0702c0f250
Tuomas Tolvanen <tutol@eg.fi>
2024-12-04 14:00:13 +0200

    Permission service

---
+++ PermissionService/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Starting PermissionService");
+
+while (true)
+{
+   Console.WriteLine("Doing stuff...");
+   Thread.Sleep(TimeSpan.FromSeconds(5));
+}
